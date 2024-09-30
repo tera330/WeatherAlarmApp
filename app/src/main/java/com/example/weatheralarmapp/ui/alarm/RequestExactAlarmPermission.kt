d38e085c13c896ce150d69cfb1620b237391f69c
tera330 <tera877tech@gmail.com>
2024-10-01 00:14:08 +0900

    正確なアラームの権限をリクエストするダイアログとインテント呼び出しを作成

---
+++ app/src/main/java/com/example/weatheralarmapp/ui/alarm/RequestExactAlarmPermission.kt
@@ -0,0 +1,58 @@
+package com.example.weatheralarmapp.ui.alarm
+
+import android.content.Intent
+import android.os.Build
+import android.provider.Settings
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.weatheralarmapp.R
+import com.example.weatheralarmapp.ui.common.Dialog
+
+@Composable
+fun RequestExactAlarmPermission(
+    modifier: Modifier = Modifier,
+    openDialog: MutableState<Boolean>,
+) {
+    val context = LocalContext.current
+
+    Column(
+        modifier =
+            modifier
+                .fillMaxSize(),
+    ) {
+        if (openDialog.value && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            Dialog(
+                onDismissRequest = {
+                    openDialog.value = false
+                },
+                onConfirmation = {
+                    openDialog.value = false
+                    val intent = Intent(Settings.ACTION_REQUEST_SCHEDULE_EXACT_ALARM)
+                    context.startActivity(intent)
+                },
+                dialogTitle = stringResource(R.string.allow_permission_set_alarm),
+                dialogText = stringResource(R.string.request_exact_alarm_permission),
+                icon = Icons.Default.Info,
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewRequestExactAlarmPermission() {
+    val openDialog = remember { mutableStateOf(true) }
+    RequestExactAlarmPermission(
+        openDialog = openDialog,
+    )
+}
---
+++ app/src/main/java/com/example/weatheralarmapp/ui/common/Dialog.kt
@@ -0,0 +1,67 @@
+package com.example.weatheralarmapp.ui.common
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.weatheralarmapp.R
+
+@Composable
+fun Dialog(
+    onDismissRequest: () -> Unit,
+    onConfirmation: () -> Unit,
+    dialogTitle: String,
+    dialogText: String,
+    icon: ImageVector,
+) {
+    AlertDialog(
+        icon = {
+            Icon(icon, contentDescription = null)
+        },
+        title = {
+            Text(text = dialogTitle)
+        },
+        text = {
+            Text(text = dialogText)
+        },
+        onDismissRequest = {
+            onDismissRequest()
+        },
+        confirmButton = {
+            TextButton(
+                onClick = {
+                    onConfirmation()
+                },
+            ) {
+                Text(text = stringResource(R.string.confirm))
+            }
+        },
+        dismissButton = {
+            TextButton(
+                onClick = {
+                    onDismissRequest()
+                },
+            ) {
+                Text(text = stringResource(R.string.Cancel))
+            }
+        },
+    )
+}
+
+@Preview
+@Composable
+fun DialogPreview() {
+    Dialog(
+        onDismissRequest = { },
+        onConfirmation = { },
+        dialogTitle = "Title",
+        dialogText = "Text",
+        icon = Icons.Default.Info,
+    )
+}
